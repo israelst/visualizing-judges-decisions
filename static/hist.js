413a1a0478f8a54226eef0cf823a7be864eee9b0
Israel Teixeira <israelst@gmail.com>
2015-04-20 23:32:40 -0300

    JSHint: use singlequote and add missing semicolon

--- static/hist.js
+++ static/hist.js
@@ -8,12 +8,12 @@ exports.hist = function hist(response){
     });
 
     function brushed(){
-        var range = brush.extent();
-        var brushed_decisions = decisions.filter(function(decision, index, array){
-            return decision.positions.some(function(pos){
-                return pos >= range[0] && pos <= range[1];
+        var range = brush.extent(),
+            brushed_decisions = decisions.filter(function(decision){
+                return decision.positions.some(function(pos){
+                    return pos >= range[0] && pos <= range[1];
+                });
             });
-        });
         document.getElementById('decisions').innerHTML = ('<div class="alert alert-success">Mostrando <strong>' +
                                                          brushed_decisions.length + '</strong> decisões.</div>');
 
@@ -24,7 +24,7 @@ exports.hist = function hist(response){
     }
 
     // A formatter for counts.
-    var formatCount = d3.format(",.0f");
+    var formatCount = d3.format(',.0f');
 
     var margin = {top: 10, right: 30, bottom: 30, left: 30},
         width = 960 - margin.left - margin.right,
@@ -36,7 +36,7 @@ exports.hist = function hist(response){
 
     var brush = d3.svg.brush()
         .x(x)
-        .on("brushend", brushed);
+        .on('brushend', brushed);
 
     // Generate a histogram using twenty uniformly-spaced bins.
     var data = d3.layout.histogram()
@@ -49,42 +49,42 @@ exports.hist = function hist(response){
 
     var xAxis = d3.svg.axis()
         .scale(x)
-        .tickFormat(d3.format(".0%"))
-        .orient("bottom");
+        .tickFormat(d3.format('.0%'))
+        .orient('bottom');
 
-    var svg = d3.select("#histogram").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select('#histogram').append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    var bar = svg.selectAll(".bar")
+    var bar = svg.selectAll('.bar')
         .data(data)
-        .enter().append("g")
-        .attr("class", "bar")
-        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+        .enter().append('g')
+        .attr('class', 'bar')
+        .attr('transform', function(d) { return 'translate(' + x(d.x) + ',' + y(d.y) + ')'; });
 
-    bar.append("rect")
-        .attr("x", 1)
-        .attr("width", x(data[0].dx) - 1)
-        .attr("height", function(d) { return height - y(d.y); });
+    bar.append('rect')
+        .attr('x', 1)
+        .attr('width', x(data[0].dx) - 1)
+        .attr('height', function(d) { return height - y(d.y); });
 
-    bar.append("text")
-        .attr("dy", ".75em")
-        .attr("y", 6)
-        .attr("x", x(data[0].dx) / 2)
-        .attr("text-anchor", "middle")
+    bar.append('text')
+        .attr('dy', '.75em')
+        .attr('y', 6)
+        .attr('x', x(data[0].dx) / 2)
+        .attr('text-anchor', 'middle')
         .text(function(d) { return formatCount(d.y); });
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
+    svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + height + ')')
         .call(xAxis);
 
-    svg.append("g")
-        .attr("class", "x brush")
+    svg.append('g')
+        .attr('class', 'x brush')
         .call(brush)
-        .selectAll("rect")
-        .attr("y", -6)
-        .attr("height", height + 7);
-}
+        .selectAll('rect')
+        .attr('y', -6)
+        .attr('height', height + 7);
+};
