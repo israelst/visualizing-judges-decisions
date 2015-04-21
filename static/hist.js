78980e30c26cf4bdd0a59346df00db98089c9835
Israel Teixeira <israelst@gmail.com>
2015-04-21 16:16:20 -0300

    Change back to doublequote to better compliance to patters

--- static/hist.js
+++ static/hist.js
@@ -1,4 +1,4 @@
-var d3 = require('d3');
+var d3 = require("d3");
 
 exports.hist = function hist(response){
     var decisions = response.decisions;
@@ -14,17 +14,17 @@ exports.hist = function hist(response){
                     return pos >= range[0] && pos <= range[1];
                 });
             });
-        document.getElementById('decisions').innerHTML = ('<div class="alert alert-success">Mostrando <strong>' +
-                                                         brushed_decisions.length + '</strong> decisões.</div>');
+        document.getElementById("decisions").innerHTML = ("<div class='alert alert-success'>Mostrando <strong>" +
+                                                         brushed_decisions.length + "</strong> decisões.</div>");
 
-        var pre = d3.select('#decisions').selectAll('pre').data(brushed_decisions);
-        pre.enter().append('pre');
+        var pre = d3.select("#decisions").selectAll("pre").data(brushed_decisions);
+        pre.enter().append("pre");
         pre.exit().remove();
         pre.text(function(d){ return d.text;});
     }
 
     // A formatter for counts.
-    var formatCount = d3.format(',.0f');
+    var formatCount = d3.format(",.0f");
 
     var margin = {top: 10, right: 30, bottom: 30, left: 30},
         width = 960 - margin.left - margin.right,
@@ -36,7 +36,7 @@ exports.hist = function hist(response){
 
     var brush = d3.svg.brush()
         .x(x)
-        .on('brushend', brushed);
+        .on("brushend", brushed);
 
     // Generate a histogram using twenty uniformly-spaced bins.
     var data = d3.layout.histogram()
@@ -49,42 +49,42 @@ exports.hist = function hist(response){
 
     var xAxis = d3.svg.axis()
         .scale(x)
-        .tickFormat(d3.format('.0%'))
-        .orient('bottom');
+        .tickFormat(d3.format(".0%"))
+        .orient("bottom");
 
-    var svg = d3.select('#histogram').append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    var svg = d3.select("#histogram").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var bar = svg.selectAll('.bar')
+    var bar = svg.selectAll(".bar")
         .data(data)
-        .enter().append('g')
-        .attr('class', 'bar')
-        .attr('transform', function(d) { return 'translate(' + x(d.x) + ',' + y(d.y) + ')'; });
+        .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
 
-    bar.append('rect')
-        .attr('x', 1)
-        .attr('width', x(data[0].dx) - 1)
-        .attr('height', function(d) { return height - y(d.y); });
+    bar.append("rect")
+        .attr("x", 1)
+        .attr("width", x(data[0].dx) - 1)
+        .attr("height", function(d) { return height - y(d.y); });
 
-    bar.append('text')
-        .attr('dy', '.75em')
-        .attr('y', 6)
-        .attr('x', x(data[0].dx) / 2)
-        .attr('text-anchor', 'middle')
+    bar.append("text")
+        .attr("dy", ".75em")
+        .attr("y", 6)
+        .attr("x", x(data[0].dx) / 2)
+        .attr("text-anchor", "middle")
         .text(function(d) { return formatCount(d.y); });
 
-    svg.append('g')
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(0,' + height + ')')
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
-    svg.append('g')
-        .attr('class', 'x brush')
+    svg.append("g")
+        .attr("class", "x brush")
         .call(brush)
-        .selectAll('rect')
-        .attr('y', -6)
-        .attr('height', height + 7);
+        .selectAll("rect")
+        .attr("y", -6)
+        .attr("height", height + 7);
 };
